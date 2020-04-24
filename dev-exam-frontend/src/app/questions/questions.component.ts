ae52a64dd4b7c260a8efb0b89fd63935a936a34d
doctorget <samuelsoares177778@gmail.com>
2020-04-24 11:50:12 -0300

    relogio com estilo e minutos inplementado

--- dev-exam-frontend/src/app/questions/questions.component.ts
+++ dev-exam-frontend/src/app/questions/questions.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { QuestionsService } from 'src/app/services/questions.service'
 import { ActivatedRoute } from '@angular/router'
 import { NgForm } from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-questions',
@@ -12,24 +13,42 @@ import { NgForm } from '@angular/forms';
 export class QuestionsComponent implements OnInit {
 
   questions: any
-  timeLeft: number = 65;
+  timeLeft: number = 1800;
   interval;
-
+  minute: number
+  second: number
+  time: any
   constructor(private questionsService: QuestionsService, private router: Router, private route: ActivatedRoute) {
+  }
+
+
+  twoHouses(value) {
+    if (value <= 9) {
+      value = "0" + value
+    }
+    return value
+  }
+
+  format(s) {
+    this.minute = this.twoHouses(Math.trunc((s % 3600) / 60))
+    this.second = this.twoHouses((s % 3600) % 60)
+
+    return this.minute + ":" + this.second;
   }
 
   startTimer() {
     this.interval = setInterval(() => {
-      if(this.timeLeft > 0) {
+      if (this.timeLeft > 0) {
         this.timeLeft--;
+        this.time = this.format(this.timeLeft)
       } else {
         this.timeLeft = 60;
       }
-      if(this.timeLeft == 0){
+      if (this.timeLeft == 0) {
         let element: HTMLElement = document.getElementById('submit') as HTMLElement;
         element.click();
       }
-    },1000)
+    }, 1000)
   }
 
   ngOnInit() {
@@ -39,15 +58,20 @@ export class QuestionsComponent implements OnInit {
         console.log(questions)
         this.questions = questions
       });
-      this.startTimer()
+    this.startTimer()
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1) {
+        $('.timer').fadeIn('slow');
+      }
+    });
+
   }
 
   onSubmit(value: any) {
     this.questionsService.sendAnswers(JSON.stringify(value))
     this.router.navigateByUrl('/success');
   }
-
-
 
 
 }
