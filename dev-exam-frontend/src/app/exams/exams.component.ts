799ef65dff1a0834f7c1e88d0087ce209e8d9026
brenombastos <brenodead@gmail.com>
2020-04-24 14:42:36 -0300

    Feita configuração da compenente exame

--- dev-exam-frontend/src/app/exams/exams.component.ts
+++ dev-exam-frontend/src/app/exams/exams.component.ts
@@ -15,11 +15,46 @@ export class ExamsComponent implements OnInit {
   
   constructor(private questionsService: QuestionsService, private router: Router, private route: ActivatedRoute) {
   }
+
+     id = 0
+
+        RemoveLinhaTabela(item) {
+                var tr = (item).closest('tr');
+                tr.fadeOut(400, function() {
+                tr.remove();  
+                });
+                return false;
+        }
+        addLine(){
+            var novaLinha = ("<tr>");
+            var itemCelula = "";
+            var testName = ('#testName');
+            var technology = ('#technology');
+            var complexity = ('#complexity');
+            var testAmount = ('#testAmount');
+            itemCelula += '<td><input readonly name="testName[]" style="border:none;" value="'+testName+'"></td>';
+            itemCelula += '<td><input name="technology[]" readonly style="border:none;" value="'+technology+'"></td>';
+            itemCelula += '<td><input name="complexity[]" readonly style="border:none;" value="'+complexity+'"></td>';
+            itemCelula += '<td><input name="testAmount[]" readonly style="border:none;" value="'+testAmount+'"></td>';
+            itemCelula += '<td class="text-center">';
+            itemCelula += '<button type="button" onclick="RemoveLinhaTabela(this)" class="btn-danger btn btn-xs">';
+            itemCelula += '<i class="fa fa-lg fa-close"></i>';
+            itemCelula += '</button>';
+            itemCelula += '</td>';
+            novaLinha.append(itemCelula);
+            ("#tabela-pedido").append(novaLinha);
+            return false;
+            }
+
+
+
+
   ngOnInit() {
     this.questionsService.questions()
       .subscribe(questions => {
         console.log(questions)
 
+        
         
         this.questions = questions
         for (let question of questions) {
