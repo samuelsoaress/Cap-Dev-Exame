834ae1a2c9e78c6f928a76ae6d91ea49d64bc5c6
doctorget <samuelsoares177778@gmail.com>
2020-05-04 16:48:07 -0300

    login adicionado, faltando apenas validação de credenciais

--- dev-exam-frontend/src/app/home/signin/signin.component.ts
+++ dev-exam-frontend/src/app/home/signin/signin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms'
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from '../../core/platform-detector/platform-detector.service';
 
 @Component({
   selector: 'app-signin',
@@ -9,8 +12,12 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms'
 export class SignInComponent implements OnInit {
 
   loginForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) { }
+  
+  constructor(
+      private formBuilder: FormBuilder,
+      private authService: AuthService,
+      private router: Router,
+      private platformDetectorService: PlatformDetectorService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -18,5 +25,21 @@ export class SignInComponent implements OnInit {
       password: ['', Validators.required]
   });
   }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+        .authenticate(userName, password)
+        .subscribe(
+            () => this.router.navigate(['manager', userName]),
+            err => {
+                console.log(err);
+                this.loginForm.reset();
+                alert('Invalid user name or password');
+            }
+        );
+}
 
 }
