8f03bbedef1dd145cced822a693035ea4b42c191
doctorget <samuelsoares177778@gmail.com>
2020-05-06 16:01:28 -0300

    logica de login recebendo requests e devolvendo status de acesso

--- dev-exam-backend/src/app.js
+++ dev-exam-backend/src/app.js
@@ -1,11 +1,13 @@
 const questions = require('./questions')
 const exams = require('./exams')
-const sessionLogin = require('./login')
+const session = require('express-session')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const config = require('config')
 const express = require('express')
 const app = express()
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const MAX_QUESTIONS = config.get('Config.api.maxQuestions')
 
@@ -15,6 +17,56 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // Enable cors
 app.options('*', cors())
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        if(username === "admin" && password === "admin"){
+            return done(null, username);
+        } else {
+            return done("unauthorized access", false);
+        }
+    }
+));
+passport.serializeUser(function(user, done) {
+    if(user) done(null, user);
+});
+  
+passport.deserializeUser(function(id, done) {
+    done(null, id);
+});
+
+app.use(session({ secret: 'anything', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const auth = () => {
+    return (req, res, next) => {
+        passport.authenticate('local', (error, user, info) => {
+            if(error) res.status(400).json({"statusCode" : 200 ,"message" : error});
+            req.login(user, function(error) {
+                if (error) return next(error);
+                next();
+            });
+        })(req, res, next);
+    }
+}
+
+app.post('/authenticate', auth() , (req, res) => {
+    res.status(200).json({"statusCode" : 200 ,"user" : req.user});
+});
+
+const isLoggedIn = (req, res, next) => {
+    console.log('session ', req.session);
+    if(req.isAuthenticated()){
+        //console.log('user ', req.session.passport.user)
+        return next()
+    }
+    return res.status(400).json({"statusCode" : 400, "message" : "not authenticated"})
+}
+
+app.get('/getData', isLoggedIn, (req, res) => {
+    res.json("data is")
+})
 
 app.get('/exam', cors(), (req, res, next) => {
 
--- dev-exam-backend/src/login.js
+++ dev-exam-backend/src/login.js
@@ -1,37 +1,66 @@
 const fs = require('fs')
 const path = require('path')
 const uuid = require('uuid/v4');
-const sessao = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+
+
+var user = {}
 
 module.exports = (app) => {
 
     passport.use(new LocalStrategy(
         {
-            usernameField: 'email',
+            usernameField: 'userName',
             passwordField: 'senha'
         },
         (email, senha, done) => {
             users = loadUsersFromfile()
             let autorizado = false
-            for(let i=0; i<users.length; i++){
-                if (!response['userName'] === users[i].login || response['password'] === users[i].password){
-                    return done(null, false, {
-                        mensagem: 'Login e/ou senha incorretos!'
-                    });
+            for (let i = 0; i < users.length; i++) {
+
+                if (response['userName'] === users[i].login && response['password'] === users[i].password) {
+                    return done(null, users[i]);
+                    user['userName'] = users[i].login
                 }
-                return done(null, users[i]);
             }
+            return done(null, false, {
+                mensagem: 'Login e/ou senha incorretos!'
+            });
+
         }
     ));
-}   
 
-const getCredentials = (response) =>{
+    passport.serializeUser((usuario, done) => {
+
+        const usuarioSessao = {
+            nome: user['userName']
+        };
+
+        done(null, usuarioSessao);
+    });
+
+    passport.deserializeUser((usuarioSessao, done) => {
+        done(null, usuarioSessao);
+    });
+
+    app.use(sessao({
+        secret: 'cap exame',
+        genid: function(req) {
+            return uuid();
+        }, 
+        resave: false,
+        saveUninitialized: false
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
+
+const getCredentials = (response) => {
     users = loadUsersFromfile()
     let autorizado = false
-    for(let i=0; i<users.length; i++){
-        if (response['userName'] == users[i].login && response['password'] === users[i].password){
+    for (let i = 0; i < users.length; i++) {
+        if (response['userName'] == users[i].login && response['password'] === users[i].password) {
             autorizado = true
         }
     }
