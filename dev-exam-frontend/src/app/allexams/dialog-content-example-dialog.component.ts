79a8784d518a49b9fcb482c3916171e624d5330a
doctorget <samuelsoares177778@gmail.com>
2020-06-01 17:28:45 -0300

    add component de popup

---
+++ dev-exam-frontend/src/app/allexams/dialog-content-example-dialog.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+
+@Component({
+    selector: 'dialog-content-example-dialog',
+    templateUrl: 'dialog-content-example-dialog.html',
+})
+export class DialogContentExampleDialog { }
