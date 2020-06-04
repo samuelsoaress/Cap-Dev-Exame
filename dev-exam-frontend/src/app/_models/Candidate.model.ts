90bbfdde80632e20f808b99603de48dcb9b09e19
doctorget <samuelsoares177778@gmail.com>
2020-06-04 17:41:49 -0300

    login e logout funcionando na tela manager, exam

---
+++ dev-exam-frontend/src/app/_models/Candidate.model.ts
@@ -0,0 +1,5 @@
+export class Candidate {
+  nome:string;
+  email: string;
+  password: string;
+  }
--- dev-exam-frontend/src/app/menu-superior/menu-superior.component.ts
+++ dev-exam-frontend/src/app/menu-superior/menu-superior.component.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/auth/auth.service';
+import { Candidate } from '../_models/Candidate.model';
+
 
 @Component({
   selector: 'app-menuSuperior',
@@ -7,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-superior.component.scss']
 })
 export class MenuSuperiorComponent implements OnInit {
-
-  constructor( private http: HttpClient) { }
-
+  currentUser: Candidate;
+  constructor(private router: Router,
+    private authenticationService: AuthService) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
   ngOnInit() {
+  }
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
