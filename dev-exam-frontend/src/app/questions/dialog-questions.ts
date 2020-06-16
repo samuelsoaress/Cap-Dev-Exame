b04e5eabb325c6f0c85e7fc554f9e2ba6778eaff
doctorget <samuelsoares177778@gmail.com>
2020-06-16 17:14:27 -0300

    implementação dialog ok faltando data

---
+++ dev-exam-frontend/src/app/questions/dialog-questions.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+
+@Component({
+  selector: 'dialog-questions',
+  templateUrl: 'dialog-questions.html',
+})
+
+export class DialogQuestions {}
